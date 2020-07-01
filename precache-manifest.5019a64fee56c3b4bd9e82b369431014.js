7946d3531787b63ae24b473adc86c01e1130ed19
Ramakrishna Shastri <ramakrishna.shastri@target.com>
2020-07-01 23:13:49 +0530

    Updates

---
+++ precache-manifest.5019a64fee56c3b4bd9e82b369431014.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e7ec891aa38444ecccbb5b4e3d4d68",
+    "url": "/isolation-board-game/index.html"
+  },
+  {
+    "revision": "bed68d288fe76476f7b9",
+    "url": "/isolation-board-game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5a4d6683485f1c6889f9",
+    "url": "/isolation-board-game/static/css/main.070e0e4f.chunk.css"
+  },
+  {
+    "revision": "bed68d288fe76476f7b9",
+    "url": "/isolation-board-game/static/js/2.5f5f45b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/isolation-board-game/static/js/2.5f5f45b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4d6683485f1c6889f9",
+    "url": "/isolation-board-game/static/js/main.de4a8b4f.chunk.js"
+  },
+  {
+    "revision": "1ab9354040bf44e66bc6",
+    "url": "/isolation-board-game/static/js/runtime-main.acc47293.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/isolation-board-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
