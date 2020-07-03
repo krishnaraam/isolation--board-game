7294f7b2e2e529d640ee158f489cab5e312e056e
Ramakrishna Shastri <ramakrishna.shastri@target.com>
2020-07-03 10:58:26 +0530

    Adding app drawer

--- src/App.js
+++ src/App.js
@@ -2,26 +2,11 @@ import React from 'react';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { Paper, Tabs, Tab } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import AppDrawer from './components/AppDrawer';
 
 export default function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  const useStyles = makeStyles({
-    root: {
-      flexGrow: 1,
-    },
-  });
-
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)');
   const theme = React.useMemo(
     () =>
       createMuiTheme({
@@ -37,20 +22,7 @@ export default function App() {
       <BrowserRouter>
        <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <Paper className={classes.root}>
-      <Tabs 
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
-        >
-         <Tab value="/game" style={{fontWeight: "bold", color: "white"}} label="Isolation Game" />
-          <Tab style={{fontWeight: "bold", color: "white"}} label="Rules" />
-          <Tab style={{fontWeight: "bold", color: "white"}} label="About" />
-          
-        </Tabs>
-      </Paper>
+      <AppDrawer></AppDrawer>
     </ThemeProvider>
 
       <Switch>
